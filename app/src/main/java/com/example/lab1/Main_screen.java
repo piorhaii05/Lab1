30bb9c7a42690699eb55d4f3114108b571c66bf3
Nguyen Hoang Hai <hoanghai20051203@gmail.com>
2024-11-05 19:54:42 +0700

    Lab

--- app/src/main/java/com/example/lab1/Main_screen.java
+++ app/src/main/java/com/example/lab1/Main_screen.java
@@ -1,6 +1,7 @@
 package com.example.lab1;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Main_screen extends AppCompatActivity {
+
+    private TextView txtHai;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,6 @@ public class Main_screen extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        txtHai = findViewById(R.id.txtHai);
     }
 }
